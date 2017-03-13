610b134738e6287eab847b11a62e00fd4de1ea97
zero-cool <andre.marques@fatec.sp.gov.br>
2017-03-13 14:29:45 -0300

    Releasing the first version
    Push is a program to send notifications over your Android device using the SimplePush app.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='shempush',
+    version='0.1',
+    packages=[''],
+    url='http://github.com/0x00-0x00/push',
+    license='MIT',
+    author='shemhazai',
+    author_email='andre.marques@fatec.sp.gov.br',
+    description='Simplepush API script to send messages',
+    scripts=["bin/push"]
+)
